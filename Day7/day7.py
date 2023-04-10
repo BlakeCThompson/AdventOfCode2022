65e627d657acf43c0ee5a719200e994eabdc097c
Blake Thompson <blake.thompson@unicity.com>
2023-04-09 19:11:53 -0600

    finish code for day 7

--- Day7/day7.py
+++ Day7/day7.py
@@ -1,7 +1,10 @@
-
-def backDirectory(path :str):
+def backDirectory(path: str):
     path = path.split('/')
-    parentPathSegments = path[1:-1:]
+
+    if path[-1] == '':
+        parentPathSegments = path[1:-2:]
+    else:
+        parentPathSegments = path[1:-1:]
     newPath = '/'
     for elem in parentPathSegments:
         newPath = newPath + elem + '/'
@@ -32,16 +35,17 @@ def handleLS(currentDirectory: str, dataStream, directories: dict):
     :return:
     """
     line = peekLine(dataStream)
-    if line.startswith('$ '):
+    nextLineIsACommand = line.startswith('$ ')
+    if nextLineIsACommand:
         return
     line = dataStream.readline().strip('\n')
     while line:
         segments = line.split(' ')
         firstSegment = segments[0]
-        firstSegmentIsInt, value = isInt(firstSegment)
-        if firstSegmentIsInt:
-            secondSegment = segments[1]
-            directories[currentDirectory + secondSegment] = value
+        thisLineIsAFileStartingWithItsSize, value = isInt(firstSegment)
+        if thisLineIsAFileStartingWithItsSize:
+            fileName = segments[1]
+            directories[currentDirectory + fileName] = value
         line = peekLine(dataStream)
         if line.startswith('$ '):
             return
@@ -55,18 +59,27 @@ def isInt(value):
     return True, i
 
 
+def addFileSizeToAllParentDirectories(size, fileName, directorySizes):
+    parentDir = backDirectory(fileName)
+    if parentDir != '/':
+        parentDir = parentDir[:-1]
+    if parentDir not in directorySizes.keys():
+        directorySizes[parentDir] = size
+    else:
+        directorySizes[parentDir] += size
+    if parentDir != '/':
+        addFileSizeToAllParentDirectories(size, parentDir, directorySizes)
+
+
 def getSumsByDirectory(files):
-    directories = {}
-    for file in files:
-        parentDir = backDirectory(file)
-        if parentDir not in directories.keys():
-            directories[parentDir] = files[file]
-        else:
-            directories[parentDir] += files[file]
-    return directories
+    directorySizes = {}
+    for fileName in files:
+        size = files[fileName]
+        addFileSizeToAllParentDirectories(size, fileName, directorySizes)
+    return directorySizes
 
 
-def getDirectoriesHavingSizeLessThanOrEqualToX(directoriesToCheck: dict, maxSize: int):
+def getDirectoriesHavingSizeLessThanOrEqualToX(directoriesToCheck: dict, maxSizeX: int):
     """
     :param directoriesToCheck: a dictionary of string->dictionary key value pairs.
         every dictionary key value in the inner dictionary is a filename -> size.
@@ -77,10 +90,18 @@ def getDirectoriesHavingSizeLessThanOrEqualToX(directoriesToCheck: dict, maxSize
     """
     qualifyingDirectories = {}
     for directory in directoriesToCheck:
-        if directoriesToCheck[directory] > maxSize:
+        if directoriesToCheck[directory] <= maxSizeX:
             qualifyingDirectories[directory] = directoriesToCheck[directory]
     return qualifyingDirectories
 
+
+
+def getDirectoriesHavingSizeGreaterThanOrEqualToX(directoriesToCheck: dict, minimumSizeX: int):
+    qualifyingDirectories = {}
+    for directory in directoriesToCheck:
+        if directoriesToCheck[directory] >= minimumSizeX:
+            qualifyingDirectories[directory] = directoriesToCheck[directory]
+    return qualifyingDirectories
 
 def getSumOfDirectories(directories):
     sumOfDirectories = 0
@@ -96,9 +117,8 @@ def peekLine(fileObj):
     return line
 
 
-def part1():
-    print("part 1:")
-    data = open("Day7/testData.txt", "r")
+def getFilesFromCommandOutputs(fileNameOfCommandOutputs):
+    data = open(fileNameOfCommandOutputs, "r")
     directories = {}
     currentDirectory = ''
     x = data.tell()
@@ -111,17 +131,45 @@ def part1():
         elif line.startswith('$ ls'):
             handleLS(currentDirectory, data, directories)
         line = data.readline()
+    return directories
+
+
+def part1(fileName):
+    print("part 1:")
+    files = getFilesFromCommandOutputs(fileName)
     directorySizeLimit = 100000
-    directories = getSumsByDirectory(directories)
+    directories = getSumsByDirectory(files)
     qualifyingDirectories = getDirectoriesHavingSizeLessThanOrEqualToX(directories, directorySizeLimit)
+    print('directories having less than ' + str(directorySizeLimit) + ": " + qualifyingDirectories.__str__())
     directoriesSum = getSumOfDirectories(qualifyingDirectories)
     print("sum of directory sizes having size >= " + str(directorySizeLimit) + '= ' + str(directoriesSum))
 
 
-def part2():
+
+
+def part2(fileName):
     print("part 2:")
 
+    files = getFilesFromCommandOutputs(fileName)
+    directories = getSumsByDirectory(files)
 
-part1()
+    totalSystemMemory = 70000000
+    print("Total system memory = " + str(totalSystemMemory))
+    usedMemory = directories['/']
 
-part2()
+    print("used memory: " + str(usedMemory))
+    availableMemory = totalSystemMemory - usedMemory
+    print("available memory: " + str(availableMemory))
+    neededMemory = 30000000
+    print("needed memory: " + str(neededMemory))
+    amountOfMemoryNeedingFreed = neededMemory - availableMemory
+    qualifyingDirectories = getDirectoriesHavingSizeGreaterThanOrEqualToX(directories, amountOfMemoryNeedingFreed)
+    sortedDirectoriesBySize = dict(sorted(qualifyingDirectories.items(), key=lambda item: item[1]))
+    print("directory having smallest size that releases enough memory: " + str(list(sortedDirectoriesBySize.items())[0][1]))
+
+fileName = "Day7/data.txt"
+# fileName = "Day7/testData.txt"
+
+part1(fileName)
+
+part2(fileName)
