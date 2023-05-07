cbafa877b8a6f94702ab5e7d02d083cbd0931f80
Blake Thompson <blake.thompson@unicity.com>
2023-05-07 00:05:50 -0600

    day11 part 1 working

--- Day11/day11.py
+++ Day11/day11.py
@@ -5,10 +5,9 @@ from typing import TextIO
 from abc import ABC
 
 
-
 class Test(ABC):
-    def executeTest(self, value):
-        pass
+    def testPasses(self, value) -> bool:
+        return False
 
 
 class TestDivisibleBy(Test):
@@ -17,19 +16,19 @@ class TestDivisibleBy(Test):
         divisor = testString.split('Test: divisible by ')[1]
         self.divisor = int(divisor)
 
-    def executeTest(self, value) -> bool:
+    def testPasses(self, value) -> bool:
         return value % self.divisor == 0
 
 
 class Operation:
     _floatingValue = 0
+
     def __init__(self, operationString):
         self.operationString = operationString
 
     def executeOperation(self, value):
         operation = self.operationString.split('Operation:')[1]
         return self._execute(operation, value)
-
 
     def _execute(self, command: str, value: int):
         self._floatingValue = value
@@ -40,26 +39,89 @@ class Operation:
         return self._floatingValue
 
 
+class MonkeyGroup:
+    def __init__(self):
+        self.monkeys = []
+    def addMonkey(self, newMonkey: Monkey) -> None:
+        newMonkey.monkeyGroup = self
+        for monkey in self.monkeys:
+            if newMonkey.trueMonkeyIndex == monkey.index:
+                newMonkey.setTrueMonkey(monkey)
+            if newMonkey.falseMonkeyIndex == monkey.index:
+                newMonkey.setFalseMonkey(monkey)
+        self.monkeys.append(newMonkey)
+
+    def executeOnAll(self) -> None:
+        for monkey in self.monkeys:
+            monkey.execute()
+
+    def getInspectedCounts(self) -> list:
+        inspectedCounts = []
+        for monkey in self.monkeys:
+            inspectedCounts.append((monkey.index, monkey.getNumberOfItemsInspected()))
+        return inspectedCounts
+
+    def getMonkeyByIndex(self, index):
+        for monkey in self.monkeys:
+            if monkey.index == index:
+                return monkey
 
 class Monkey:
-    _index = 0
-    def __init__(self, items: list, operation: Operation, test: Test):
-        self.index = Monkey._index
-        Monkey._index += 1
-        self.items = items
+
+    def __init__(self, index: int, items: list, operation: Operation, test: Test, trueMonkeyIndex, falseMonkeyIndex):
+        self.falseMonkey = None
+        self.trueMonkey = None
+        self.trueMonkeyIndex = trueMonkeyIndex
+        self.falseMonkeyIndex = falseMonkeyIndex
+        self.index = index
+        self.itemWorryLevels = items
         self.operation = operation
         self.test = test
+        self.inspectedItems = []
+        self.monkeyGroup = None
 
-    def execute(self):
-        for index, item in enumerate(self.items):
-            self.items[index] = self.operation.executeOperation(item)
+    def addItem(self, itemValue: int) -> None:
+        self.itemWorryLevels.append(itemValue)
 
-    def setTrueMonkey(self, monkey: Monkey):
+    def lookForTrueMonkey(self):
+        for monkey in self.monkeyGroup.monkeys:
+            if monkey.index == self.trueMonkeyIndex:
+                self.trueMonkey = monkey
+                return monkey
+
+    def lookForFalseMonkey(self):
+        for monkey in self.monkeyGroup.monkeys:
+            if monkey.index == self.falseMonkeyIndex:
+                self.falseMonkey = monkey
+                return monkey
+
+    def execute(self) -> None:
+        itemsCopy = self.itemWorryLevels.copy()
+        for item in itemsCopy:
+            self.inspectedItems.append(item)
+            item = self.operation.executeOperation(item)
+            reliefModifierAfterInspection = 3
+            item = item/reliefModifierAfterInspection
+            item = int(item)
+            if self.test.testPasses(item):
+                if self.trueMonkey is None:
+                    self.lookForTrueMonkey()
+                self.trueMonkey.addItem(item)
+            else:
+                if self.falseMonkey is None:
+                    self.lookForFalseMonkey()
+                self.falseMonkey.addItem(item)
+            firstElement = 0
+            self.itemWorryLevels.pop(firstElement)
+
+    def setTrueMonkey(self, monkey: Monkey) -> None:
         self.trueMonkey = monkey
 
-
-    def setFalseMonkey(self, monkey: Monkey):
+    def setFalseMonkey(self, monkey: Monkey) -> None:
         self.falseMonkey = monkey
+
+    def getNumberOfItemsInspected(self) -> int:
+        return len(self.inspectedItems)
 
 
 def parseMonkeyStrings(fileName: TextIO):
@@ -76,18 +138,69 @@ def parseMonkeyStrings(fileName: TextIO):
             currentMonkeyString = ''
     return monkeyStrings
 
-def parseMonkeyObjectFromString(monkeyString) -> Monkey:
+
+def parseMonkeyIndexFromString(trueMonkeyString: str):
+
+    return readIntegerFromString(trueMonkeyString.split('onkey ')[1])
+
+def readIntegerFromString(string: str ):
+    num_str = ""
+    for char in string:
+        if char.isdigit():
+            num_str += char
+        else:
+            break
+    if num_str == "":
+        return None
+    else:
+        return int(num_str)
+
+def parseMonkeyObjectFromString(monkeyString: str) -> Monkey:
+    monkeyString = monkeyString.strip()
+    monkeyLines = monkeyString.split('\n')
+    monkeyIndex = parseMonkeyIndexFromString(monkeyLines[0])
+    startingItems = parseStartingItems(monkeyLines[1])
+    operation = Operation(monkeyLines[2])
+    test = TestDivisibleBy(monkeyLines[3])
+    trueIndex = parseMonkeyIndexFromString(monkeyLines[4])
+    falseIndex = parseMonkeyIndexFromString(monkeyLines[5])
+
+    return Monkey(monkeyIndex, startingItems, operation, test, trueIndex, falseIndex)
+
+
+def parseStartingItems(startingItemsString):
+    startingItemsString = startingItemsString.split('items: ')[1]
+    startingItemsHolder = startingItemsString.split(', ')
+    startingItems = []
+    for item in startingItemsHolder:
+        startingItems.append(int(item))
+    return startingItems
+
+
+def getTopInspectingMonkeys(monkeyGroup: MonkeyGroup, topCount: int) -> list:
+    topMonkeys = []
     pass
+    # for monkeyIndex, monkey in monkeyGroup.getInspectedCounts():
 
 
+def executeOnAllXTimes(monkeyGroup: MonkeyGroup, numberOfTimes: int=20):
+    for iteration in range(numberOfTimes):
+        monkeyGroup.executeOnAll()
 
 def part1(fileName):
-    monkeys = parseMonkeyStrings(fileName)
+    ioStream = open(fileName, 'r')
+    monkeyStrings = parseMonkeyStrings(ioStream)
+    monkeyGroup = MonkeyGroup()
+    for monkeyString in monkeyStrings:
+        monkeyGroup.addMonkey(parseMonkeyObjectFromString(monkeyString))
+    numberOfIterations = 20
+    executeOnAllXTimes(monkeyGroup, numberOfIterations)
+    inspections = monkeyGroup.getInspectedCounts()
+    inspections = sorted(inspections, key=lambda x: x[1], reverse=True)
+    print('top 2 by inspections:', inspections[0], inspections[1])
+    print('monkey business level = ' + str( inspections[0][1]*inspections[1][1]))
 
-    # print(stringImage)
-
-
-# file = 'Day10/testData.txt'
+# file = 'Day11/testData.txt'
 file = 'Day11/data.txt'
-# part1(file)
+part1(file)
 # part2(file)
--- Day11/day11Tests.py
+++ Day11/day11Tests.py
@@ -1,5 +1,4 @@
-import Day11.day11
-import Day11.day11 as day11
+import day11
 
 import unittest
 
@@ -33,30 +32,165 @@ class TestDay11(unittest.TestCase):
     If true: throw to monkey 0
     If false: throw to monkey 1'''
         monkey0 = day11.parseMonkeyObjectFromString(monkeyString0)
-        # self.assertIsInstance(monkey0.test, day11.Test)
+        self.assertIsInstance(monkey0.test, day11.Test)
 
     def testTestDivisibleBy(self):
-        testDivisibleBy = Day11.day11.TestDivisibleBy('Test: divisible by 17')
-        self.assertTrue(testDivisibleBy.executeTest(17))
-        self.assertTrue(testDivisibleBy.executeTest(34))
-        self.assertFalse(testDivisibleBy.executeTest(15))
-        self.assertFalse(testDivisibleBy.executeTest(20))
+        testDivisibleBy = day11.TestDivisibleBy('Test: divisible by 17')
+        self.assertTrue(testDivisibleBy.testPasses(17))
+        self.assertTrue(testDivisibleBy.testPasses(34))
+        self.assertFalse(testDivisibleBy.testPasses(15))
+        self.assertFalse(testDivisibleBy.testPasses(20))
 
 
     def testOperation(self):
         operation = day11.Operation('Operation: new = old + 3')
-        testDivisibleBy17 = Day11.day11.TestDivisibleBy('Test: divisible by 17')
+        testDivisibleBy17 = day11.TestDivisibleBy('Test: divisible by 17')
 
-        monkey = day11.Monkey([15, 14], operation, testDivisibleBy17)
-        monkey.execute()
-        self.assertEqual(18, monkey.items[0])
-        self.assertEqual(17, monkey.items[1])
-        monkey.execute()
-        self.assertEqual(21, monkey.items[0])
+        monkey1 = day11.Monkey(0, [15, 48], operation, testDivisibleBy17, 1, 2)
+        self.assertEqual(0, monkey1.getNumberOfItemsInspected())
+        monkey2 = day11.Monkey(1, [], day11.Operation('Operation: new = old * 2'), day11.TestDivisibleBy('Test: divisible by 5'), 0, 2)
+        monkey3 = day11.Monkey(2, [], day11.Operation('Operation: new = old / 3'), day11.TestDivisibleBy('Test: divisible by 4'), 0, 1)
+        monkey1.setTrueMonkey(monkey2)
+        monkey1.setFalseMonkey(monkey3)
+        monkey1.execute()
+
+        self.assertEqual(2, monkey1.getNumberOfItemsInspected())
+
+        self.assertEqual(0, len(monkey1.itemWorryLevels), 'monkey should have passed item 1 to monkey3 and item 2 to monkey2')
+        self.assertEqual(6, monkey3.itemWorryLevels[0])
+        self.assertEqual(17, monkey2.itemWorryLevels[0])
+        monkey1.execute()
+        self.assertEqual(0, len(monkey1.itemWorryLevels))
+
+    def testParseStartingItems(self):
+        startingItemsString = '  Starting items: 71, 56, 50, 73'
+        startingItems = day11.parseStartingItems(startingItemsString)
+        self.assertEqual(4, len(startingItems))
+        self.assertEqual(71, startingItems[0])
+        self.assertEqual(56, startingItems[1])
+        self.assertEqual(50, startingItems[2])
+        self.assertEqual(73, startingItems[3])
+    def getMonkeyGroupOfThree(self):
+        monkeyGroup = day11.MonkeyGroup()
+        monkeyString0 = '''Monkey 0:
+                  Starting items: 1, 2, 3
+                  Operation: new = old + 1
+                  Test: divisible by 3
+                    If true: throw to monkey 1
+                    If false: throw to monkey 2
+                '''
+        monkey0 = day11.parseMonkeyObjectFromString(monkeyString0)
+        monkeyString1 = '''Monkey 1:
+                  Starting items: 63
+                  Operation: new = old + 3
+                  Test: divisible by 6
+                    If true: throw to monkey 0
+                    If false: throw to monkey 2'''
+        monkey1 = day11.parseMonkeyObjectFromString(monkeyString1)
+        monkeyString2 = '''Monkey 2:
+                  Starting items: 74
+                  Operation: new = old + 3
+                  Test: divisible by 6
+                    If true: throw to monkey 0
+                    If false: throw to monkey 1'''
+        monkey2 = day11.parseMonkeyObjectFromString(monkeyString2)
+        monkeyGroup.addMonkey(monkey0)
+        monkeyGroup.addMonkey(monkey1)
+        monkeyGroup.addMonkey(monkey2)
+        return monkeyGroup
+    def testMonkeyGroup(self):
+        monkeyGroup = self.getMonkeyGroupOfThree()
+        self.assertEqual(3, len(monkeyGroup.monkeys))
+
+    def testParseMonkeyIndexFromString(self):
+        trueString = '            If true: throw to monkey 0'
+        index = day11.parseMonkeyIndexFromString(trueString)
+        self.assertEqual(0, index)
+
+    def testParseFalseMonkeyIndex(self):
+        falseString = '            If false: throw to monkey 1'
+        index = day11.parseMonkeyIndexFromString(falseString)
+        self.assertEqual(1, index)
 
 
+    def testMonkeyGroupExecute(self):
+        monkeyGroup = day11.MonkeyGroup()
+        monkeyString0 = '''Monkey 0:
+                          Starting items: 1, 2, 3
+                          Operation: new = old + 1
+                          Test: divisible by 3
+                            If true: throw to monkey 1
+                            If false: throw to monkey 2
+                        '''
+        monkey0 = day11.parseMonkeyObjectFromString(monkeyString0)
+        monkeyString1 = '''Monkey 1:
+                          Starting items: 63
+                          Operation: new = old + 3
+                          Test: divisible by 6
+                            If true: throw to monkey 0
+                            If false: throw to monkey 2'''
+        monkey1 = day11.parseMonkeyObjectFromString(monkeyString1)
+        monkeyString2 = '''Monkey 2:
+                          Starting items: 74
+                          Operation: new = old + 3
+                          Test: divisible by 6
+                            If true: throw to monkey 0
+                            If false: throw to monkey 1'''
+        monkey2 = day11.parseMonkeyObjectFromString(monkeyString2)
+        monkeyGroup.addMonkey(monkey0)
+        monkeyGroup.addMonkey(monkey1)
+        monkeyGroup.addMonkey(monkey2)
+        monkeyGroup.executeOnAll()
+        self.assertEqual(monkeyGroup.monkeys[0].inspectedItems, [1, 2, 3])
+        self.assertEqual(monkeyGroup.monkeys[1].inspectedItems, [63])
+        self.assertEqual(monkeyGroup.monkeys[2].inspectedItems, [74])
+        monkeyGroup.executeOnAll()
 
-
+    def testGetMonkeyByIndex(self):
+        monkeyGroup = day11.MonkeyGroup()
+        monkeyString0 = '''Monkey 0:
+                  Starting items: 1, 2, 3
+                  Operation: new = old + 1
+                  Test: divisible by 3
+                    If true: throw to monkey 1
+                    If false: throw to monkey 2
+                '''
+        monkey0 = day11.parseMonkeyObjectFromString(monkeyString0)
+        monkeyString1 = '''Monkey 1:
+                  Starting items: 63
+                  Operation: new = old + 3
+                  Test: divisible by 6
+                    If true: throw to monkey 0
+                    If false: throw to monkey 2'''
+        monkey1 = day11.parseMonkeyObjectFromString(monkeyString1)
+        monkeyString2 = '''Monkey 2:
+                  Starting items: 74
+                  Operation: new = old + 3
+                  Test: divisible by 6
+                    If true: throw to monkey 0
+                    If false: throw to monkey 1'''
+        monkey2 = day11.parseMonkeyObjectFromString(monkeyString2)
+        monkeyGroup.addMonkey(monkey0)
+        monkeyGroup.addMonkey(monkey1)
+        monkeyGroup.addMonkey(monkey2)
+        monkeyIndex = 2
+        foundMonkey = monkeyGroup.getMonkeyByIndex(2)
+        self.assertEqual(foundMonkey.index, monkeyIndex)
+        monkeyIndex = 1
+        foundMonkey = monkeyGroup.getMonkeyByIndex(monkeyIndex)
+        self.assertEqual(foundMonkey.index, monkeyIndex)
+        monkeyIndex = 0
+        foundMonkey = monkeyGroup.getMonkeyByIndex(monkeyIndex)
+        self.assertEqual(foundMonkey.index, monkeyIndex)
+    def testMain(self):
+        fileName = 'testData.txt'
+        ioStream = open(fileName, 'r')
+        monkeyStrings = day11.parseMonkeyStrings(ioStream)
+        monkeyGroup = day11.MonkeyGroup()
+        for monkeyString in monkeyStrings:
+            monkeyGroup.addMonkey(day11.parseMonkeyObjectFromString(monkeyString))
+        day11.executeOnAllXTimes(monkeyGroup)
+        self.assertEqual(101, len(monkeyGroup.monkeys[0].inspectedItems))
 
 if __name__ == '__main__':
     unittest.main()
