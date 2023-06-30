0479dba9199d4c1f1f1a75dd62e92ff38aece447
Blake Thompson <blake.thompson@unicity.com>
2023-06-30 15:17:13 -0600

    day13 better tests for part 1

--- Day13/day13.py
+++ Day13/day13.py
@@ -1,14 +1,101 @@
 import json
 
+from utils.enum import enum
+
+
 class PacketSet:
     def __init__(self, leftPacket: list, rightPacket: list):
         self.leftPacket = leftPacket
         self.rightPacket = rightPacket
 
+    def calculateIfPacketsAreInCorrectOrder(self) -> bool:
+        """
+        :return: true if packets are in correct order, otherwise false.
+        """
+        index = 0
+        comparisonResult = 0
+        while True:
+            if index >= len(self.leftPacket):
+                return comparisonResult < 1
+            if index >= len(self.rightPacket):
+                return False
+            comparisonResult = self.__packetComparisonHelper(self.leftPacket[index], self.rightPacket[index])
+            index += 1
+            if comparisonResult == 0:
+                continue
+            return comparisonResult < 1
+        return comparisonResult < 1
 
-def parseLeftAndRightPackets(packetsString: str):
+    def __packetComparisonHelper(self, leftElement, rightElement):
+        """
+        :param leftElement: could be a list or integer.
+        :param rightElement: could be a list or integer.
+        :return: if leftElement is less than rightElement, return -1.
+                 if leftElement is equal to rightElement, return 0.
+                 if leftElement is greater than rightElement, return 1.
+        """
+        if isinstance(leftElement, list) and not isinstance(rightElement, list):
+            rightElement = [rightElement]
+            return self.__compareLists(leftElement, rightElement)
+        elif isinstance(rightElement, list) and not isinstance(leftElement, list):
+            leftElement = [leftElement]
+            return self.__compareLists(leftElement, rightElement)
+        elif isinstance(leftElement, list) and isinstance(rightElement, list):
+            return self.__compareLists(leftElement, rightElement)
+        else:
+            return self.__compareIntegers(leftElement, rightElement)
+
+
+    def __compareIntegers(self, leftInteger, rightInteger):
+        if leftInteger < rightInteger:
+            return -1
+        elif leftInteger == rightInteger:
+            return 0
+        else:
+            return 1
+
+
+    def __compareLists(self, leftList, rightList):
+        """
+        :param leftList:
+        :param rightList:
+        :return: if leftList is less than rightList, return -1.
+                 if leftList is equal to rightList, return 0.
+                 if leftList is greater than rightList, return 1.
+        """
+        index = 0
+        for index, _ in enumerate(leftList):
+            if len(rightList) == 0:
+                return 1
+            comparisonResult = self.__packetComparisonHelper(leftList[index], rightList[index])
+            index += 1
+            if comparisonResult == -1:
+                return -1
+            if comparisonResult == 0:
+                continue
+            if comparisonResult == 1:
+                return 1
+        return 0
+
+
+def parsePacketSet(packetsString: str):
     packetsStrings = packetsString.split('\n')
     leftPacket = json.loads(packetsStrings[0])
     rightPacket = json.loads(packetsStrings[1])
     return PacketSet(leftPacket, rightPacket)
 
+def parsePacketsFromFile(fileName: str):
+    result = []
+    with open(fileName, 'r') as file:
+        sublist = []
+        for line in file:
+            line = line.strip()
+            if line:
+                sublist.append(line)
+            else:
+                if sublist:
+                    result.append(parsePacketSet(sublist[0] + '\n' + sublist[1]))
+                    sublist = []
+        if sublist:
+            result.append(parsePacketSet(sublist[0] + '\n' + sublist[1]))
+    return result
--- Day13/day13Tests.py
+++ Day13/day13Tests.py
@@ -5,9 +5,17 @@ import unittest
 
 # When comparing two values, the first value is called left and the second value is called right. Then:
 #
-# If both values are integers, the lower integer should come first. If the left integer is lower than the right integer, the inputs are in the right order. If the left integer is higher than the right integer, the inputs are not in the right order. Otherwise, the inputs are the same integer; continue checking the next part of the input.
-# If both values are lists, compare the first value of each list, then the second value, and so on. If the left list runs out of items first, the inputs are in the right order. If the right list runs out of items first, the inputs are not in the right order. If the lists are the same length and no comparison makes a decision about the order, continue checking the next part of the input.
-# If exactly one value is an integer, convert the integer to a list which contains that integer as its only value, then retry the comparison. For example, if comparing [0,0,0] and 2, convert the right value to [2] (a list containing 2); the result is then found by instead comparing [0,0,0] and [2].
+# If both values are integers, the lower integer should come first.
+#   If the left integer is lower than the right integer, the inputs are in the right order.
+#   If the left integer is higher than the right integer, the inputs are not in the right order.
+#       Otherwise, the inputs are the same integer; continue checking the next part of the input.
+#   If both values are lists, compare the first value of each list, then the second value, and so on.
+#       If the left list runs out of items first, the inputs are in the right order.
+#       If the right list runs out of items first, the inputs are not in the right order.
+#       If the lists are the same length and no comparison makes a decision about the order, continue checking the next part of the input.
+#       If exactly one value is an integer, convert the integer to a list which contains that integer as its only value, then retry the comparison.
+# For example, if comparing [0,0,0] and 2, convert the right value to [2] (a list containing 2);
+# the result is then found by instead comparing [0,0,0] and [2].
 
 
 class Day13Tests(unittest.TestCase):
@@ -15,8 +23,70 @@ class Day13Tests(unittest.TestCase):
 
     def testParsePackets(self):
         packetsString = '[1,1,3,1,1]\n[1,1,5,1,1]'
-        packets = day13.parseLeftAndRightPackets(packetsString)
+        packets = day13.parsePacketSet(packetsString)
         leftPacket = packets.leftPacket
         self.assertListEqual(leftPacket, [1, 1, 3, 1, 1])
         rightPacket = packets.rightPacket
         self.assertListEqual(rightPacket, [1, 1, 5, 1, 1])
+        packetsString = '[1,[2,[3,[4,[5,6,7]]]],8,9]\n[1,[2,[3,[4,[5,6,0]]]],8,9]'
+        packets = day13.parsePacketSet(packetsString)
+        leftPacket = packets.leftPacket
+        self.assertListEqual(leftPacket, [1, [2, [3, [4, [5, 6, 7]]]], 8, 9])
+        rightPacket = packets.rightPacket
+        self.assertListEqual(rightPacket, [1,[2,[3,[4,[5,6,0]]]],8,9])
+        self.assertFalse(packets.calculateIfPacketsAreInCorrectOrder())
+
+    def testParsePackets2(self):
+        packetsString = '[[1],[2,3,4]]\n[[1],4]'
+        packetSet = day13.parsePacketSet(packetsString)
+        self.assertTrue(packetSet.calculateIfPacketsAreInCorrectOrder())
+
+    def testParsePackets3(self):
+        packetsString = '[9]\n[[8,7,6]]'
+        packetSet = day13.parsePacketSet(packetsString)
+        self.assertFalse(packetSet.calculateIfPacketsAreInCorrectOrder())
+
+    def testParsePackets4(self):
+        packetsString = '[[4,4],4,4]\n[[4,4],4,4,4]'
+        packetSet = day13.parsePacketSet(packetsString)
+        self.assertTrue(packetSet.calculateIfPacketsAreInCorrectOrder())
+
+    def testParsePackets5(self):
+        packetsString = '[7,7,7,7]\n[7,7,7]'
+        packetSet = day13.parsePacketSet(packetsString)
+        self.assertFalse(packetSet.calculateIfPacketsAreInCorrectOrder())
+
+    def testParsePackets6(self):
+        packetsString = '[]\n[3]'
+        packetSet = day13.parsePacketSet(packetsString)
+        self.assertTrue(packetSet.calculateIfPacketsAreInCorrectOrder())
+
+    def testParsePackets7(self):
+        packetsString = '[[[]]]\n[[]]'
+        packetSet = day13.parsePacketSet(packetsString)
+        self.assertFalse(packetSet.calculateIfPacketsAreInCorrectOrder())
+
+    def testParsePackets8(self):
+        packetsString = '[1,[2,[3,[4,[5,6,7]]]],8,9]\n[1,[2,[3,[4,[5,6,0]]]],8,9]'
+        packetSet = day13.parsePacketSet(packetsString)
+        self.assertFalse(packetSet.calculateIfPacketsAreInCorrectOrder())
+
+    def testParsePacketsFile(self):
+        packetFile = 'testData.txt'
+        packetSets = day13.parsePacketsFromFile(packetFile)
+        self.assertEqual(8, len(packetSets))
+
+    def testComparePackets(self):
+        packetFile = 'testData.txt'
+        packetSets = day13.parsePacketsFromFile(packetFile)
+        results = []
+        for packetSet in packetSets:
+            results.append(packetSet.calculateIfPacketsAreInCorrectOrder())
+        self.assertTrue(results[0])
+        self.assertTrue(results[1])
+        self.assertFalse(results[2])
+        self.assertTrue(results[3])
+        self.assertFalse(results[4])
+        self.assertTrue(results[5])
+        self.assertFalse(results[6])
+        self.assertFalse(results[7])
